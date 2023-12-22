160ece2c5b84037a42094598eed2ce986339fe97
memoowi <megatenlike@gmail.com>
2023-12-22 07:21:51 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="./" element={<Home />} />
+        <Route path="./about" element={<About />} />
       </Routes>
     </BrowserRouter>
   )
